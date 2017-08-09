71dfcb3e7beb3f49bc22d40a3e96d3a2c8b35d20
Ludovico Iovino <iovinoludovico@users.noreply.github.com>
2017-08-09 18:45:21 +0200

    initial commit

---
+++ application/helpers/login_helper.php
@@ -0,0 +1,34 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+| -------------------------------------------------------------------
+|  Helper for accessing the UserLogin object
+| -------------------------------------------------------------------
+|
+|   Provides access to the UserLogin object
+|
+|	Usage:
+|       $this->load->helper('login'); //load this helper
+|       $val = getUserLogin(); //retrieve UserLogin object
+|
+|   Implementation:
+|       The UserLogin object will not be created until requested for the first time.
+|       When it is requested for the first time it is created and stored in the session.
+|       
+*/
+
+    function getUserLogin() {
+        $CI = &get_instance();
+    
+        // sometimes necessary to load the user model explictily
+        $CI->load->model('user');
+        
+        $userlogin = $CI->latesession->get('USERLOGIN');
+        if (!isset($userlogin)||($userlogin==null)) {
+            $userlogin = new UserLogin();
+            $CI->latesession->set('USERLOGIN',$userlogin);
+        }
+        return $userlogin;
+    }
+
+?>
